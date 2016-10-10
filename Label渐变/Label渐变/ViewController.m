b109b0779436082870bc34477b0a62913195c46a
付云磊 <1053591151@qq.com>
2016-10-10 11:59:35 +0800

    适配
    添加了约束

--- Label渐变/Label渐变/ViewController.m
+++ Label渐变/Label渐变/ViewController.m
@@ -6,6 +6,11 @@
 //  Copyright © 2016年 eparking2. All rights reserved.
 //
 
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width     //屏幕宽度
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height    //屏幕高度
+
+#define kLayerWidth (SCREEN_WIDTH/4)
+
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -17,23 +22,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
     _maskLayer = [[CALayer alloc] init];
-    _maskLayer.frame = CGRectMake(0, 10, 100, 40);
-//    _maskLayer.cornerRadius = 20;
+    _maskLayer.frame = CGRectMake(0, 10, kLayerWidth, 40);
+    //    _maskLayer.cornerRadius = 20;
     _maskLayer.backgroundColor = [UIColor blackColor].CGColor;
     self.viewBg.layer.mask = _maskLayer;
     
 }
-- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
     UITouch *touch = [touches anyObject];
-   CGPoint point = [touch  locationInView:self.view];
+    CGPoint point = [touch  locationInView:self.view];
     
     CGFloat centerX = point.x;
     
-    _maskLayer.frame = CGRectMake(centerX-50, 10, 100, 40);
+    _maskLayer.frame = CGRectMake(centerX-kLayerWidth/2, 10, kLayerWidth, 40);
     
+}
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    if (_maskLayer.frame.origin.x<0) {
+        _maskLayer.frame = CGRectMake(0, 10, kLayerWidth, 40);
+    }else if (_maskLayer.frame.origin.x + _maskLayer.frame.size.width > SCREEN_WIDTH)
+    {
+        _maskLayer.frame = CGRectMake(SCREEN_WIDTH - kLayerWidth, 10, kLayerWidth, 40);
+    }
+}
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch  locationInView:self.view];
+    
+    CGFloat centerX = point.x;
+    
+    _maskLayer.frame = CGRectMake(centerX-kLayerWidth/2, 10, kLayerWidth, 40);
 }
 
 - (void)didReceiveMemoryWarning {
